e8421972e23e82e3210b0ca48d6ba73e038f5040
SeongJunChun <csj991210@naver.com>
2023-10-29 06:14:14 +0900

    feat:팔로우 도메인 생성

---
+++ src/main/java/sw_semester/todolist/entity/Follow.java
@@ -0,0 +1,40 @@
+package sw_semester.todolist.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@DynamicUpdate
+@Entity
+public class Follow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn
+    private User follower; // 팔로우를 하는 사람
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn
+    private User followee; // 팔로우를 받는 사람
+
+    @Builder
+    public Follow(User follower, User followee) {
+        this.follower = follower;
+        this.followee = followee;
+    }
+
+    public void disconnectFollower() {
+        this.follower = null;
+    }
+
+    public void disconnectFollowee() {
+        this.followee = null;
+    }
+}
