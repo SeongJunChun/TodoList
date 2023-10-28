0b84333cb4e8125e1d1f2177e45d5661ab996005
SeongJunChun <csj991210@naver.com>
2023-10-29 06:08:50 +0900

    refactor:자료형 변환(int->long)

--- src/main/java/sw_semester/todolist/loginpackage/token/TokenRepository.java
+++ src/main/java/sw_semester/todolist/loginpackage/token/TokenRepository.java
@@ -1,4 +1,4 @@
-package sw_semester.todolist.token;
+package sw_semester.todolist.loginpackage.token;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -6,13 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 import java.util.Optional;
 
-public interface TokenRepository extends JpaRepository<Token,Integer> {
+public interface TokenRepository extends JpaRepository<Token,Long> {
 
 
     @Query("select t from Token t inner join User u " +
            "on t.user.id = u.id " +
            "where u.id = :id and (t.expired = false or t.revoked = false)")
-    List<Token> findAllValidTokenByUser(Integer id);
+    List<Token> findAllValidTokenByUser(Long id);
 
     Optional<Token> findByToken(String token);
 
