f6b5a52f0adc447f0a4fb67a5d732ae8134fec1b
SeongJunChun <csj991210@naver.com>
2023-12-09 03:03:00 +0900

    git ignore add

--- src/main/java/sw_semester/todolist/TodolistApplication.java
+++ src/main/java/sw_semester/todolist/TodolistApplication.java
@@ -2,6 +2,7 @@ package sw_semester.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -9,7 +10,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class TodolistApplication {
 	public static void main(String[] args) {
-
 		SpringApplication.run(TodolistApplication.class, args);
 	}
 }
