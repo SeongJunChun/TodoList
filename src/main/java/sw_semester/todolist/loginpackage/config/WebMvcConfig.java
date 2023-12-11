179020a43389f3f28652bb52c9f3fba5eea56114
SeongJunChun <csj991210@naver.com>
2023-12-11 18:59:51 +0900

    FEAT:cors허용 메소드 option추가

--- src/main/java/sw_semester/todolist/loginpackage/config/WebMvcConfig.java
+++ src/main/java/sw_semester/todolist/loginpackage/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5174")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(true);
     }
